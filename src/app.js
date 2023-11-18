d42b6a412171842fc47e7cfe8ede2e5e7ed1c711
virgo <aerynn@null.net>
2023-11-19 00:46:10 +1300

    removed frivolous port redundancy

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
